1b557429772762e46f6981e3c7479bfa76093640
961105114@qq.com <t>
2020-05-25 19:51:16 +0800

    eureka集群配置

---
+++ cloud-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wangJiaLun
+ * @date 2020-05-24
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
